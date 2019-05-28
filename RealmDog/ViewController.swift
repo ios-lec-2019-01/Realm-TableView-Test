08adcce8860b4046056ccf5eff2928650596e5f5
Jonghyun Kim <jhkim@dit.ac.kr>
2019-05-28 16:30:08 +0900

    Initial Commit

---
+++ RealmDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmDog
+//
+//  Created by 김종현 on 28/05/2019.
+//  Copyright © 2019 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
